dd0b69a04af2245b6a43f54810f03d038a9d0ef4
lydhr <lydhr@sina.com>
2014-04-12 13:26:35 +0800

    beta 1.1

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -127,7 +127,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "dabendan hahaha!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
